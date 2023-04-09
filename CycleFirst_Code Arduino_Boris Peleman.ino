91c4a251c8facc4fc87dfc8a7f227303cd9149b1
BorisPeleman <85952865+BorisPeleman@users.noreply.github.com>
2023-04-09 16:55:02 +0200

    Add files via upload

---
+++ CycleFirst_Code Arduino_Boris Peleman.ino
@@ -0,0 +1,198 @@
+#include <Bike_detection_V2_inferencing.h> //Plaats hier je Edge Impuls Library
+#include "esp_camera.h"
+#include <WiFi.h>
+
+#define CAMERA_MODEL_AI_THINKER
+#define Red_Bike 2
+#define Red_Car 14
+#define Green_Bike 4
+#define Green_Car 15
+
+#include "img_converters.h"
+#include "image_util.h"
+#include "esp_camera.h"
+#include "camera_pins.h"
+
+const char* ssid = "*********";
+const char* password = "*********";
+
+dl_matrix3du_t *resized_matrix = NULL;
+ei_impulse_result_t result = {0};
+
+void startCameraServer();
+// setup
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  pinMode(Red_Bike, OUTPUT);
+  pinMode(Red_Car, OUTPUT);
+  pinMode(Green_Bike, OUTPUT); 
+  pinMode(Green_Car, OUTPUT); 
+  // camere configureren
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_240X240;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);
+    s->set_brightness(s, 1); 
+    s->set_saturation(s, 0); 
+  }
+
+  Serial.println("Camera Ready!");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() { 
+
+  // Maak een foto en classificeer
+  String result = classify();
+
+  Serial.printf("Result: %s\n", result); 
+  delay(2000); 
+  Serial.printf("Result: %s\n", result);
+  if (result.equals("Bike")) { 
+  digitalWrite(Red_Car, HIGH);
+  digitalWrite(Green_Car, LOW);
+  digitalWrite(Red_Bike, LOW);
+  digitalWrite(Green_Bike, HIGH);
+  } else if (result.equals("Other")) {
+  digitalWrite(Red_Car, LOW);
+  digitalWrite(Green_Car, HIGH);
+  digitalWrite(Red_Bike, HIGH);
+  digitalWrite(Green_Bike, LOW);  
+  }
+}
+
+String classify() {
+
+  capture_quick();
+
+  if (!capture()) return "Error";
+
+  Serial.println("Getting image...");
+  signal_t signal;
+  signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_WIDTH;
+  signal.get_data = &raw_feature_get_data;
+
+  Serial.println("Run classifier...");
+  EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false /* debug */);
+  dl_matrix3du_free(resized_matrix);
+
+  ei_printf("run_classifier returned: %d\n", res);
+  if (res != 0) return "Error";
+
+  ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+            result.timing.dsp, result.timing.classification, result.timing.anomaly);
+  int index;
+  float score = 0.0;
+  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+    if (result.classification[ix].value > score) {
+      score = result.classification[ix].value;
+      index = ix;
+    }
+    ei_printf("    %s: \t%f\r\n", result.classification[ix].label, result.classification[ix].value);
+  }
+
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+  ei_printf("    anomaly score: %f\r\n", result.anomaly);
+#endif
+  return String(result.classification[index].label);
+}
+void capture_quick() {
+  camera_fb_t *fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) return;
+  esp_camera_fb_return(fb);
+}
+bool capture() {
+
+  Serial.println("Capture image...");
+  esp_err_t res = ESP_OK;
+  camera_fb_t *fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return false;
+  }
+
+  Serial.println("Converting to RGB888...");
+  dl_matrix3du_t *rgb888_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);
+  fmt2rgb888(fb->buf, fb->len, fb->format, rgb888_matrix->item);
+
+  Serial.println("Resizing the frame buffer...");
+  resized_matrix = dl_matrix3du_alloc(1, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, 3);
+  image_resize_linear(resized_matrix->item, rgb888_matrix->item, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, 3, fb->width, fb->height);
+
+  dl_matrix3du_free(rgb888_matrix);
+  esp_camera_fb_return(fb);
+
+  return true;
+}
+
+int raw_feature_get_data(size_t offset, size_t out_len, float *signal_ptr) {
+
+  size_t pixel_ix = offset * 3;
+  size_t bytes_left = out_len;
+  size_t out_ptr_ix = 0;
+
+  while (bytes_left != 0) {
+    uint8_t r, g, b;
+    r = resized_matrix->item[pixel_ix];
+    g = resized_matrix->item[pixel_ix + 1];
+    b = resized_matrix->item[pixel_ix + 2];
+
+    float pixel_f = (r << 16) + (g << 8) + b;
+    signal_ptr[out_ptr_ix] = pixel_f;
+
+    out_ptr_ix++;
+    pixel_ix += 3;
+    bytes_left--;
+  }
+
+  return 0;
+}
